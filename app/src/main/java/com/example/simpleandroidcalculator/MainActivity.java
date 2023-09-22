cf00f5ce828f3457a97835cff041172ef5b9c8cf
Oshan <oshansandeep630@gmail.com>
2023-09-22 23:50:48 +0530

    Implemented clickListners for numeric buttons

--- app/src/main/java/com/example/simpleandroidcalculator/MainActivity.java
+++ app/src/main/java/com/example/simpleandroidcalculator/MainActivity.java
@@ -3,12 +3,118 @@ package com.example.simpleandroidcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public Double ans = null;
+    public Double number1 = 0.0;
+    public Double number2 = 0.0;
+    public Integer num;
+    public char[] operatorArray = {' ', '+', '-', '*', '/'};
+    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btn_delete, btn_eq, btn_multi, btn_div, btn_sub, btn_plus, btn_dell, btn_ac;
+    TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Toast toast = Toast.makeText(getApplicationContext(), "Syntax Eror!", Toast.LENGTH_LONG);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btn0 = findViewById(R.id.btn0);
+        btn_delete = findViewById(R.id.btn_delete);
+        btn_eq = findViewById(R.id.btn_eq);
+        btn_multi = findViewById(R.id.btn_Multi);
+        btn_div = findViewById(R.id.btn_div);
+        btn_sub = findViewById(R.id.btn_sub);
+        btn_plus = findViewById(R.id.btn_plus);
+        btn_dell = findViewById(R.id.btn_dell);
+        btn_ac = findViewById(R.id.btn_ac);
+
+        display = findViewById(R.id.txt_disp);
+
+        display.setText("");
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "1");
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "2");
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "3");
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "4");
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "5");
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "6");
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "7");
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "8");
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "9");
+            }
+        });
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(display.getText() + "0");
+            }
+        });
+
     }
 }
